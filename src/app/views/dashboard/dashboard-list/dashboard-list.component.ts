85f3eb12279c9a5b56720d35c557632ce543b5ae
Leonardo A Amante Machado <leonardo.machado@fepese.org.br>
2019-10-14 17:41:08 -0300

    Transfering group load to Dashboard-List

--- src/app/views/dashboard/dashboard-list/dashboard-list.component.ts
+++ src/app/views/dashboard/dashboard-list/dashboard-list.component.ts
@@ -4,7 +4,9 @@ import { Router } from "@angular/router";
 import { DateAdapter, MatPaginator } from "@angular/material";
 import { TranslateService } from "@ngx-translate/core";
 import { DashboardListService } from "./dashboard-list.service";
+import { LocalStorageService } from "../../../core/services";
 import { LZStringService } from "ng-lz-string";
+import {debounceTime} from "rxjs/operators";
 
 @Component({
   selector: 'dashboard-list',
@@ -20,16 +22,19 @@ export class DashboardListComponent implements OnInit, AfterViewInit {
   groupsReaderDataSource: DashboardModel.GroupsReader[] = [];
   searchTerm: string;
 
+  submitted: boolean;
+  synchronized: boolean;
   loading: boolean;
   filter: any;
 
   constructor(
-    private http: HttpClient,
-    public translate: TranslateService,
-    private dashboardListService: DashboardListService,
-    private router: Router,
-    private adapter: DateAdapter<any>,
-    private lz: LZStringService
+      private http: HttpClient,
+      public translate: TranslateService,
+      public localStorage: LocalStorageService,
+      private dashboardListService: DashboardListService,
+      private router: Router,
+      private adapter: DateAdapter<any>,
+      private lz: LZStringService
   ) {
     this.adapter.setLocale('pt-PT');
     translate.setDefaultLang('pt-br');
@@ -38,19 +43,60 @@ export class DashboardListComponent implements OnInit, AfterViewInit {
   }
 
   ngOnInit() {
-    this.getGroupReader();
+    this.loading = true;
+    this.prepareGroupReader();
     // this.translateMatPaginator();
   }
 
   ngAfterViewInit() {
   }
 
+  private prepareGroupReader() {
+    this.dashboardListService.getGroupReaderUrl();
+    this.dashboardListService.getAll()
+        .pipe()
+        .subscribe(
+            (response) => {
+              if (response.status === 200) {
+                let groups = response.data;
+                this.localStorage.setItem('groups', this.lz.compress(JSON.stringify(groups)));
+                this.getGroupReader();
+                this.loading = false;
+                let protocols = [];
+                groups.forEach(function (group) {
+                  group.protocols.forEach(function (protocol) {
+                    let protocol_local = {
+                      id:                protocol.id,
+                      date_reader:       protocol.date_reader,
+                      categories_id:     protocol.categories_id,
+                      group_reader_id:   protocol.group_reader_id,
+                      moderator_id:      protocol.moderator_id,
+                      participant_id:    protocol.participant_id,
+                      participant_name:  protocol.participant_name,
+                      protocol_type:     protocol.protocol_type,
+                      registration_code: protocol.registration_code,
+                      period:            protocol.period,
+                      active: 1,
+                      sync: 1
+                    };
+                    protocols.push(protocol_local);
+                  });
+                });
+                this.localStorage.setItem('protocols', JSON.stringify(protocols));
+                this.synchronized = true;
+                this.localStorage.setItem('synchronized', JSON.stringify(this.synchronized));
+                this.submitted = false;
+              }
+              this.router.navigate(['/']);
+            },);
+  }
+
   private getGroupReader(): void {
-      this.groupsReaderDataSource = JSON.parse(
+    this.groupsReaderDataSource = JSON.parse(
         this.lz.decompress(
-          localStorage.getItem('groups')
+            localStorage.getItem('groups')
         )
-      )
+    )
   }
 
   translateMatPaginator() {
@@ -59,10 +105,10 @@ export class DashboardListComponent implements OnInit, AfterViewInit {
       'COMPONENT.PAGINATOR.NEXT_PAGE',
       'COMPONENT.PAGINATOR.PREVIOUS_PAGE',
     ])
-      .subscribe(translation => {
-        this.paginator._intl.itemsPerPageLabel = translation['COMPONENT.PAGINATOR.ITEMS_PER_PAGE'];
-        this.paginator._intl.nextPageLabel     = translation['COMPONENT.PAGINATOR.NEXT_PAGE'];
-        this.paginator._intl.previousPageLabel = translation['COMPONENT.PAGINATOR.PREVIOUS_PAGE'];
-      });
+        .subscribe(translation => {
+          this.paginator._intl.itemsPerPageLabel = translation['COMPONENT.PAGINATOR.ITEMS_PER_PAGE'];
+          this.paginator._intl.nextPageLabel     = translation['COMPONENT.PAGINATOR.NEXT_PAGE'];
+          this.paginator._intl.previousPageLabel = translation['COMPONENT.PAGINATOR.PREVIOUS_PAGE'];
+        });
   }
 }
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -12,143 +12,103 @@ import { LZStringService } from "ng-lz-string";
 import Swal from 'sweetalert2';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
-  preserveWhitespaces: false
+    selector: 'app-dashboard',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    preserveWhitespaces: false
 })
 export class LoginComponent implements OnInit {
 
-  loginForm: FormGroup;
-  submitted: boolean;
-  hide: boolean;
-  synchronized: boolean;
-  app_version: string;
+    loginForm: FormGroup;
+    submitted: boolean;
+    hide: boolean;
+    synchronized: boolean;
+    app_version: string;
 
-  constructor(
-    private authService: AuthenticationService,
-    private formBuilder: FormBuilder,
-    private router: Router,
-    public translate: TranslateService,
-    public dashboardListService: DashboardListService,
-    public localStorage: LocalStorageService,
-    private lz: LZStringService
-  ) {
-    translate.setDefaultLang('pt-br');
-    const browserLang = translate.getBrowserLang();
-    translate.use(browserLang.match(/en|pt-br/) ? browserLang : 'pt-br');
-  }
-
-  ngOnInit() {
-    this.submitted = false;
-    this.hide      = true;
-
-    this.prepareAppVersion();
-
-    this.loginForm = this.formBuilder.group(
-      {
-        email: ['', [Validators.required, Validators.email]],
-        password: ['', Validators.required]
-      });
-  }
-
-  get fields() {
-    return this.loginForm.controls;
-  }
-
-  prepareAppVersion() : void {
-    this.app_version = environment.app_version;
-  }
-
-  onSubmit() {
-    let email    = this.loginForm.get('email').value;
-    let password = this.loginForm.get('password').value;
-
-    if (this.loginForm.valid) {
-      this.login(
-        email,
-        password
-      );
+    constructor(
+        private authService: AuthenticationService,
+        private formBuilder: FormBuilder,
+        private router: Router,
+        public translate: TranslateService,
+        public dashboardListService: DashboardListService,
+        public localStorage: LocalStorageService,
+        private lz: LZStringService
+    ) {
+        translate.setDefaultLang('pt-br');
+        const browserLang = translate.getBrowserLang();
+        translate.use(browserLang.match(/en|pt-br/) ? browserLang : 'pt-br');
     }
 
-    if (!this.loginForm.valid) {
-      Swal.fire('Ops!', 'Favor avaliar os campos obrigatórios!', 'error');
-      return;
+    ngOnInit() {
+        this.submitted = false;
+        this.hide      = true;
+
+        this.prepareAppVersion();
+
+        this.loginForm = this.formBuilder.group(
+            {
+                email: ['', [Validators.required, Validators.email]],
+                password: ['', Validators.required]
+            });
     }
-  }
 
-  private login(email, password): void {
-    let authservice = this.authService;
-    this.submitted  = true;
-    authservice.login(
-      email,
-      password
-    )
-      .pipe(debounceTime(300))
-      .subscribe(
-        (response) => {
+    get fields() {
+        return this.loginForm.controls;
+    }
 
-          authservice.currentUser(
-            response
-          )
-            .subscribe(
-              (resp_auth) => {
-                authservice.setCurrentUser(resp_auth.data);
-                this.dashboardListService.getGroupReaderUrl();
-                this.dashboardListService.getAll()
-                  .pipe(debounceTime(300))
-                  .subscribe(
-                    (response) => {
-                      if (response.status === 200) {
-                        let groups = response.data;
-                        this.localStorage.setItem('groups', this.lz.compress(JSON.stringify(groups)));
-                        let protocols = [];
-                        groups.forEach(function (group) {
-                          group.protocols.forEach(function (protocol) {
-                              let protocol_local = {
-                                  id:                protocol.id,
-                                  date_reader:       protocol.date_reader,
-                                  categories_id:     protocol.categories_id,
-                                  group_reader_id:   protocol.group_reader_id,
-                                  moderator_id:      protocol.moderator_id,
-                                  participant_id:    protocol.participant_id,
-                                  participant_name:  protocol.participant_name,
-                                  protocol_type:     protocol.protocol_type,
-                                  registration_code: protocol.registration_code,
-                                  period:            protocol.period,
-                                  active: 1,
-                                  sync: 1
-                              };
-                            protocols.push(protocol_local);
-                          });
-                        });
-                        this.localStorage.setItem('protocols', JSON.stringify(protocols));
-                        this.synchronized = true;
-                        this.localStorage.setItem('synchronized', JSON.stringify(this.synchronized));
-                        this.submitted = false;
-                      }
-                      this.router.navigate(['/'])
-                        .catch(reason => {
-                          console.warn(reason);
-                        });
-                    },
-                    error => {
-                      this.submitted = false;
-                      console.warn(error.toString())
-                    }
-                  );
-              },
-              error_auth => {
-                this.submitted = false;
-                console.warn(error_auth)
-              }
+    prepareAppVersion() : void {
+        this.app_version = environment.app_version;
+    }
+
+    onSubmit() {
+        let email    = this.loginForm.get('email').value;
+        let password = this.loginForm.get('password').value;
+
+        if (this.loginForm.valid) {
+            this.login(
+                email,
+                password
             );
-        },
-        error => {
-          this.submitted = false;
-          Swal.fire('Ops!', 'E-mail informado não existe ou a senha está incorreta!', 'error');
         }
-      );
-  }
+
+        if (!this.loginForm.valid) {
+            Swal.fire('Ops!', 'Favor avaliar os campos obrigatórios!', 'error');
+            return;
+        }
+    }
+
+    private login(email, password): void {
+        let authservice = this.authService;
+        this.submitted  = true;
+        authservice.login(
+            email,
+            password
+        )
+            .pipe(debounceTime(300))
+            .subscribe(
+                (response) => {
+
+                    authservice.currentUser(
+                        response
+                    )
+                        .subscribe(
+                            (resp_auth) => {
+                                authservice.setCurrentUser(resp_auth.data);
+                                this.synchronized = true;
+                                this.localStorage.setItem('synchronized', JSON.stringify(this.synchronized));
+                                this.router.navigate(['/']);
+                            },
+                            error_auth => {
+                                this.submitted = false;
+                                console.warn(error_auth)
+                            }
+                        );
+                },
+                error => {
+                    this.submitted = false;
+                    Swal.fire('Ops!', 'E-mail informado não existe ou a senha está incorreta!', 'error');
+                }
+            );
+    }
 
 }
