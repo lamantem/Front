02c81efc35f2f57c5dc1f96228f70c8d3d9aee34
lamantem <lamantemachado@gmail.com>
2019-11-30 01:13:07 -0300

    Editv0.1

--- src/app/views/dashboard/dashboard-create/dashboard-create.component.ts
+++ src/app/views/dashboard/dashboard-create/dashboard-create.component.ts
@@ -1,4 +1,15 @@
 import { AfterViewInit, OnInit, Component } from '@angular/core';
+import EditData = DashboardModel.EditData;
+
+
+
+const Edit_DATA: EditData[] = [
+  {user_id: '1' , question_id: '2' , statement: 'qUESTÃO ddddddddddddddddddddddddddddddddddddddddddddddddddddddd sdsd asd asd asdasdadsdsa dada djdiaosjdoia jsdoia sjdoias jdoaisdj oasidj aosidj aosidj aodij odiajsd osi1 OPA', situation: 'Em aberto', action: 'Editar'},
+  {user_id: '1' , question_id: '2' , statement: 'OUTRA QUESTATOASO', situation: 'Em aberto', action: 'Editar'},
+  {user_id: '1' , question_id: '2' , statement: 'ASDASD', situation: 'Em aberto', action: 'Editar'},
+  {user_id: '1' , question_id: '2' , statement: 'A12321ASSD', situation: 'Em avaliação', action: 'Editar'},
+  {user_id: '1' , question_id: '2' , statement: 'OOOOOASD', situation: 'Em avaliação', action: 'Editar'},
+];
 
 @Component({
   selector: 'dashboard-create',
@@ -10,6 +21,8 @@ export class DashboardCreateComponent implements OnInit, AfterViewInit {
   useruc: any;
   CurricularUnit: any;
   Course: any;
+  editdataSource: DashboardModel.EditData[] = Edit_DATA;
+  editcolumns: string[] = ['statement', 'situation', 'action'];
   constructor(
     ) {}
 
---
+++ src/app/views/dashboard/dashboard.model.ts
@@ -0,0 +1,10 @@
+declare namespace DashboardModel {
+
+    export interface EditData {
+        user_id: string;
+        question_id: string;
+        statement: string;
+        situation: string;
+        action: string;
+    }
+}
